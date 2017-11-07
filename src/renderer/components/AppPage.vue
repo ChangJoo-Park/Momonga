26dbfc22d1f8be98fa8c15bd66885c87a2e969fc
ChangJoo Park <pcjpcj2@gmail.com>
2017-11-07 21:37:39 +0900

    Remove boilerplate

---
+++ src/renderer/components/AppPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="wrapper">
+    <main>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app-page',
+    components: {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+
+</style>
