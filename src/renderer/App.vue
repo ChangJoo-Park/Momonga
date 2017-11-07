34e42dcb085e0b892f0fbf3074439213d388a18e
ChangJoo Park <pcjpcj2@gmail.com>
2017-11-07 21:29:51 +0900

    Initialize Commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'parrots'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
