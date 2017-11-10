e2894fc655bb8ca3f0f4e540f01d4a7a1f9a3163
ChangJoo Park <pcjpcj2@gmail.com>
2017-11-10 22:35:49 +0900

    Add adaptive text area

---
+++ src/renderer/components/AppPage/DayItem/AdaptiveTextarea.vue
@@ -0,0 +1,59 @@
+<template>
+  <textarea
+  v-model="text"
+  class="textarea"
+  :rows="currentRows"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  name: 'adaptive-textarea',
+  data: function () {
+    return {
+      text: '',
+      minRows: 1,
+      maxRows: 20,
+      currentRows: 1,
+      baseScrollHeight: -1,
+      fontSize: 1
+    }
+  },
+  mounted: function () {
+    this.baseScrollHeight = this.$el.scrollHeight
+    this.currentRows = this.minRows
+    this.fontSize = this.getCurrentFontSize()
+    this.$el.focus()
+  },
+  watch: {
+    text: function () {
+      this.$emit('update', this.text)
+      this.updateRows()
+    }
+  },
+  methods: {
+    updateRows: function () {
+      if (this.text === '') {
+        this.currentRows = this.minRows
+        return
+      }
+      let rows = Math.ceil((this.$el.scrollHeight - this.baseScrollHeight) / this.fontSize)
+
+      if (rows > this.maxRows) {
+        rows = this.maxRows
+      } else if (rows < this.minRows) {
+        rows = this.minRows
+      }
+      console.log('rows : => ', rows)
+      this.currentRows = rows
+    },
+    getCurrentFontSize: function () {
+      return parseInt(window.getComputedStyle(this.$el).getPropertyValue('font-size'))
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
