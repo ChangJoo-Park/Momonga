0843a7c8ea5285d196247f579fab887f15b2e124
ChangJoo Park <pcjpcj2@gmail.com>
2017-11-12 10:11:45 +0900

    Add hoodie and pouchdb

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
+import PouchDB from 'pouchdb-browser'
+import HoodieStore from '@hoodie/store-client'
 import App from './App'
 import router from './router'
 import store from './store'
@@ -7,6 +9,14 @@ import store from './store'
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+/* Initialize Database */
+const hoodieStore = new HoodieStore('momonga', {
+  PouchDB,
+  remoteBaseUrl: 'http://localhost:5984'
+})
+
+Vue.prototype.$db = hoodieStore
 
 /* eslint-disable no-new */
 new Vue({
