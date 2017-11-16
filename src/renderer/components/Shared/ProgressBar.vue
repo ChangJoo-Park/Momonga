ec903cdfeca4f569689cb58e1cbfbd0a7259d088
ChangJoo Park <pcjpcj2@gmail.com>
2017-11-16 23:56:11 +0900

    Add ProgressBar component for indicating downloading

---
+++ src/renderer/components/Shared/ProgressBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="progress-wrapper"
+    v-if="downloading">
+    <div class="progress" :style="{ width: progressWidth }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    downloading: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    percent: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  },
+  computed: {
+    progressWidth: function () {
+      return this.percent + '%'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
